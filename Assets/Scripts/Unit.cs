6a0401e2b5be2b25f0bf0599a614df4156454bb5
Sjors Jessen <sjors.jessen@gmail.com>
2017-09-07 00:32:39 +0200

    Path Smooting (1)
    Set-up work to allow path 'smoothing'

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,51 +5,33 @@ using UnityEngine;
 public class Unit : MonoBehaviour {
 
     public Transform target;
-    float speed = 20;
-    Vector3[] path;
-    int targetIndex;
+    public float speed = 20;
+    public float turnDistance = 5;
+
+    Path path;
 
     void Start(){
         PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
     }
 
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful){
+    public void OnPathFound(Vector3[] waypoints, bool pathSuccessful){
         if(pathSuccessful) {
-            path = newPath;
+            path = new Path(waypoints, transform.position, turnDistance);
             StopCoroutine("FollowPath");
             StartCoroutine("FollowPath");
         }
     }
 
     IEnumerator FollowPath(){
-        Vector3 currentWaypoint = path[0];
-
         while(true){
-        if(transform.position == currentWaypoint){
-                targetIndex++;
-                if(targetIndex >= path.Length){
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex];
-            }
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+    
             yield return null;
         }
     }
 
     public void OnDrawGizmos(){
-        if(path != null) {
-        for (int i = targetIndex; i < path.Length; i++) {
-                Gizmos.color = Color.black;
-                Gizmos.DrawCube(path[i], Vector3.one);
-
-                if (i == targetIndex){
-                    Gizmos.DrawLine(transform.position, path[i]);
-                }
-                else{
-                    Gizmos.DrawLine(path[i-1], path[i]);
-                }
-            }
+        if (path != null){
+        path.DrawWithGizmos();
         }
     }
 }
